22c94fd337e3b7d2ab555b3c3d87da617fb8d300
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-06-27 22:32:50 +0100

    debugging task 3

--- print_octal.c
+++ print_octal.c
@@ -25,21 +25,14 @@ int print_octal_helper(unsigned int num)
  */
 
 int print_octal(va_list arg)
-{
-	unsigned int num, counter = 0;
-	int n = va_arg(arg, int);
+	{
+	unsigned int num = va_arg(arg, unsigned int);
+	int counter = 0;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-		counter++;
-	}
-	else
-	{
-		num = n;
-	}
-	counter += print_octal_helper(num);
+	if (num / 8)
+		counter += print_octal_helper(num / 8);
+
+	counter += _putchar('0' + num % 8);
 
 	return (counter);
 }
